ec202a3d3335611e844944882000f3aa1fb8cd0f
ylyu <yl.yu@pulmuone.com>
2024-07-23 21:06:58 +0900

    쉴래

--- TodoList/js/totolist.js
+++ TodoList/js/totolist.js
@@ -1,2 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
+
+// input 값 입력후 list에 출력
+todoInput.addEventListener('click', function() {
+  console.log('클릭');
+  alert("Div was clicked!");
+});
+
+// list 에 출력
+
